932d500057b8fe9614ec03dd9e234469f33d5dc6
vinay898 <vinay898@gmail.com>
2019-09-16 21:55:04 +0530

    json working

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  films: Observable<any>;
+  public WalletBalance: string="";
+  constructor(public httpClient: HttpClient) {
+    this.films = this.httpClient.get('http://localhost/db.php'); //https://swapi.co/api/films
+    this.films
+    .subscribe(data => {
+      
+      this.WalletBalance = data[1].amount;
+      console.log('my data: ', JSON.stringify(data));
+  })
 
-  constructor() {}
-
+}
 }
